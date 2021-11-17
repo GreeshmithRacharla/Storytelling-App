b807b251b28933c36abf43895fb781030a4862e9
GreeshmithRacharla <76734059+GreeshmithRacharla@users.noreply.github.com>
2021-11-17 20:52:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAWHJS3g4i2d6wDzt7wzIfCkgz-mTSYkAo",
+    authDomain: "storytellingapp-5609b.firebaseapp.com",
+    projectId: "storytellingapp-5609b",
+    storageBucket: "storytellingapp-5609b.appspot.com",
+    messagingSenderId: "173419950585",
+    appId: "1:173419950585:web:4f38253c718d92081acd28"
+  };
